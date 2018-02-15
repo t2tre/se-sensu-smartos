74e02f5f3619678cb9506467c47e1c9e09c019b3
Raymond Edah <raymond.edah@secretescapes.com>
2018-02-15 11:26:45 +0000

    updated build folder reset script

--- buildscripts/clean.sh
+++ buildscripts/clean.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
 
+# Reset build folders
+
+umask 022
+
+BIN_RM=/usr/bin/rm
+BIN_MKDIR=/usr/bin/mkdir
+
+FOLDER_LIST="~sensu/log ~sensu/run ~sensu/etc ~sensu/app ~sensu/app/bin ~sensu/app/sbin ~sensu/app/vendor"
+
 echo "Resetting build folders..."
-/bin/rm -rf ~sensu/log ~sensu/run ~sensu/etc ~sensu/app
+for folder in ${FOLDER_LIST}; do
+   echo "Resetting ${folder}"
+   ${BIN_RM} -rf ${folder}
+   ${BIN_MKDIR} ${folder}
+done
