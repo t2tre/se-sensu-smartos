d078e4360038f6fd3c45ba8db02d1020233f7275
Raymond Edah <raymond.edah@secretescapes.com>
2018-02-15 11:32:56 +0000

    updated build folder reset script

--- buildscripts/clean.sh
+++ buildscripts/clean.sh
@@ -8,10 +8,11 @@ BIN_RM=/usr/bin/rm
 BIN_MKDIR=/usr/bin/mkdir
 
 FOLDER_LIST="~sensu/log ~sensu/run ~sensu/etc ~sensu/app ~sensu/app/bin ~sensu/app/sbin ~sensu/app/vendor"
+FOLDER_LIST="log run etc app app/bin app/sbin app/vendor"
 
 echo "Resetting build folders..."
 for folder in ${FOLDER_LIST}; do
    echo "Resetting ${folder}"
-   ${BIN_RM} -rf ${folder}
-   ${BIN_MKDIR} ${folder}
+   ${BIN_RM} -rf ${HOME}/${folder}
+   ${BIN_MKDIR} ${HOME}/${folder}
 done
