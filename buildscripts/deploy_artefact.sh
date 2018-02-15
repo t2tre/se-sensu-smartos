3ddb44d1ce90ec5c4f1b15cd7226373c2ec16517
Raymond Edah <raymond.edah@secretescapes.com>
2018-02-15 16:21:16 +0000

    Updated deploy script

--- buildscripts/deploy_artefact.sh
+++ buildscripts/deploy_artefact.sh
@@ -5,6 +5,8 @@ umask 022
 BIN_TAR=/usr/bin/gtar
 BIN_UNAME=/usr/bin/uname
 BIN_CURL=/opt/local/bin/curl
+BIN_SHASUM=/opt/local/bin/shasum
+BIN_MD5SUM=/ec/bin/md5sum
 
 export PATH=${HOME}/go/bin:${HOME}/app/bin:${HOME}/app/sbin:/opt/local/sbin:/opt/local/bin:$PATH
 package_file_name=sensu-`${BIN_UNAME} -s`-`${BIN_UNAME} -r`.tgz
@@ -12,6 +14,16 @@ deployment_url="${ARTIFACTORY_URL}/sensu/BUILD_${BUILD_ID}/${package_file_name}"
 
 cd ${HOME}
 
-echo "${BIN_CURL} -H \"X-JFrog-Art-Api: ${APIKEY}\" -T target/${package_file_name} ${deployment_url}"
-${BIN_CURL} -H \"X-JFrog-Art-Api: ${APIKEY}\" -T target/${package_file_name} ${deployment_url}
+SHA1SUM=$(${BIN_SHASUM} -a 1 target/${package_file_name} | awk '{print $1}')
+SHA256SUM=$(${BIN_SHASUM} -a 256 target/${package_file_name} | awk '{print $1}')
+MD5SUM=$(${BIN_MD5SUM} target/${package_file_name} | awk '{print $1}')
+
+${BIN_CURL} -XPUT \
+   -H "X-JFrog-Art-Api: ${APIKEY}" \
+   -H "X-Checksum-Md5: ${MD5SUM}" \
+   -H "X-Checksum-Sha1: ${SHA1SUM}" \
+   -H "X-Checksum-Sha256: ${SHA256SUM}" \
+   -T target/${package_file_name} ${deployment_url}
+
+# ${BIN_CURL} -XPUT -H \"X-JFrog-Art-Api: ${APIKEY}\" -T target/${package_file_name} ${deployment_url}
 
