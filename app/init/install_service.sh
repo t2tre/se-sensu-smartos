f0a7997fb5c7f177f3cf2b46bbbb8952e764114e
Raymond Edah <raymond.edah@secretescapes.com>
2018-02-16 10:55:50 +0000

    Fixed service install script

--- app/init/install_service.sh
+++ app/init/install_service.sh
@@ -5,7 +5,7 @@
 SENSU_HOME_FOLDER=`/usr/bin/su - sensu -c "/bin/bash -c \"export\"" | egrep "declare -x HOME" | cut -f 2 -d = | sed -e 's/\"//g'`
 ESC_SENSU_HOME_STR=`echo ${SENSU_HOME_FOLDER} | /usr/bin/sed -e 's/\//\\\\\//g'`
 
-/usr/bin/cat ${SENSU_HOME_FOLDER}/app/init/sensu_client_template.xml > /var/svc/manifest/site/sensu-client.xml
+/usr/bin/cat ${SENSU_HOME_FOLDER}/app/init/sensu_client_template.xml | /usr/bin/sed -e "s/SENSU_HOME/${ESC_SENSU_HOME_STR}/g" > /var/svc/manifest/site/sensu-client.xml
 /usr/bin/chmod 0644 /var/svc/manifest/site/sensu-client.xml
 /usr/sbin/svccfg validate /var/svc/manifest/site/sensu-client.xml
 if [ ${?} -eq 0 ]; then
